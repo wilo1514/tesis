0558627d5e1fc9bf8c340d58f5137da9e5cffee0
William Lituma <williamlituma072@gmail.com>
2024-07-29 11:40:08 -0500

    api gateway

--- clients/src/index.js
+++ clients/src/index.js
@@ -6,7 +6,7 @@ const clientRoutes = require('./routes/clientRoutes');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 app.use(express.json());
 app.use('/api/clients', clientRoutes);
