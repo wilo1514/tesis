2dad559ff2177522f91a0845dbeda56ee85176a8
cxzadm <113950353+cxzadm@users.noreply.github.com>
2024-10-07 21:44:52 -0500

    Front Clientes Filtros, edición, nuevo

--- front/src/components/clientes/ClientComponent.vue
+++ front/src/components/clientes/ClientComponent.vue
@@ -13,16 +13,17 @@ export default {
     return {
       clients: [],
       fields: [
+        {key: "index", label: "N"},
         {key: "identificacion", label: "Identificacion"},
         {key: "razonSocial", label: "Razón Social", sortable: true},
         {key: "email", label: "Email", sortable: true},
         {key: "telefono", label: "Teléfono"},
+        {key: "obligadoContabilidad", label: "Obligado Contabilidad", sortable: true},
         {key: "actions", label: "Acciones"},
       ],
       items: [
-        {text: 'Admin', href: '#'},
-        {text: 'Manage', href: '#'},
-        {text: 'Library', active: true}
+        {text: 'Clientes', href: '#'},
+        {text: 'Lista de clientes', active: true}
       ],
       clienteActual: {
         razonSocial: '',
@@ -55,13 +56,33 @@ export default {
       searchQuery: "",
       perPage: 10,
       currentPage: 1,
+      modalTitle: '',
     };
   },
   computed: {
+    filteredClients() {
+      if (this.searchQuery) {
+        return this.clients.filter((client) => {
+          return (
+              client.identificacion
+                  .toLowerCase()
+                  .includes(this.searchQuery.toLowerCase()) ||
+              client.razonSocial
+                  .toLowerCase()
+                  .includes(this.searchQuery.toLowerCase()) ||
+              client.email.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+              client.telefono
+                  .toLowerCase()
+                  .includes(this.searchQuery.toLowerCase())
+          );
+        });
+      }
+      return this.clients;
+    },
     paginatedClients() {
       const start = (this.currentPage - 1) * this.perPage;
       const end = start + this.perPage;
-      return this.clients.slice(start, end);
+      return this.filteredClients.slice(start, end);
     },
   },
   methods: {
@@ -88,26 +109,33 @@ export default {
         console.error("Error al obtener la lista de clientes:", error);
       }
     },
+    resetClient() {
+
+      this.clienteActual = {
+        razonSocial: '',
+        email: '',
+        telefono: '',
+        identificacion: '',
+        direccion: '',
+        tipoIdentificacion: '01',
+        contribuyenteRimpe: '',
+        obligadoContabilidad: 'NO'
+      };
+    },
+
+
     openModal(mode, client = null) {
       if (mode === 'crear') {
         this.modalTitle = 'Nuevo Cliente';
         this.editMode = false;
-        this.clienteActual = {
-          razonSocial: '',
-          email: '',
-          telefono: '',
-          identificacion: '',
-          direccion: '',
-          tipoIdentificacion: '01',
-          contribuyenteRimpe: '',
-          obligadoContabilidad: 'NO'
-        };
-      } else if (mode === 'editar') {
+        this.resetClient()
+      } else {
         this.modalTitle = 'Editar Cliente';
         this.editMode = true;
         this.clienteActual = {...client}; // Cargar los datos del cliente
       }
-      this.$bvModal.show('modal-nuevo-cliente');
+
+
     },
     async saveClient() {
       try {
@@ -116,8 +144,12 @@ export default {
         } else {
           await createClient(this.clienteActual); // Crear nuevo cliente
         }
-        this.$bvModal.hide('modal-nuevo-cliente');
+        this.resetClient()
+        this.hideModal();
+
         this.fetchClients(); // Recargar la lista de clientes
+
+
       } catch (error) {
         console.error("Error al guardar el cliente:", error);
       }
@@ -130,10 +162,39 @@ export default {
         console.error("Error al eliminar el cliente:", error);
       }
     },
+    showModalNew() {
+
+      this.editMode = false;
+      this.resetClient();
+      console.log("abrir modal", this.clienteActual)
+      this.$refs['my-modal'].show()
+    },
+    showModalEdit(data) {
+
+      this.editMode = true;
+      this.clienteActual = {...data};
+      console.log("abrir modal", this.clienteActual)
+      this.$refs['my-modal'].show()
+    },
+    hideModal() {
+      this.editMode = false;
+      this.resetClient();
+      this.$refs['my-modal'].hide()
+      console.log("ocultar modal", this.clienteActual)
+    },
+    toggleModal() {
+      // We pass the ID of the button that we want to return focus to
+      // when the modal has hidden
+      this.$refs['my-modal'].toggle('#toggle-btn')
+    }
+
   },
-  created () {
+
+  created() {
     this.fetchClients();
   },
+
+
 };
 </script>
 
@@ -147,42 +208,73 @@ export default {
       <div>
         <h2 class="mb-0 text-primary">Lista de Clientes</h2>
       </div>
-      <b-button variant="primary" class="align-self-center" @click="openModal('crear')">Nuevo cliente</b-button>
+      <div>
+
+        <b-button variant="primary" class="align-self-center m-2" @click="showModalNew">Nuevo cliente</b-button>
+
+      </div>
     </div>
 
 
     <div class="d-flex justify-content-center w-100">
-      <div class="row w-100 d-flex justify-content-center align-items-center">
-        <div class="col-6">
-          <!-- Se cambia el evento @input para que busque en tiempo real -->
-          <b-form-input v-model="searchQuery" @input="fetchClients"
-                        placeholder="Buscar por razón social o identificación"
-                        class="m-1"></b-form-input>
-        </div>
-        <div class="col-1">
-          <!-- Botón de reset de búsqueda -->
+      <b-row class="w-100 d-flex justify-content-center align-items-center">
+        <b-col lg="3"></b-col>
+        <b-col lg="6">
+          <b-form-input
+              v-model="searchQuery"
+              placeholder="Buscar por razón social, identificación, email o teléfono"
+              class="m-1"
+          ></b-form-input>
+        </b-col>
+        <b-col lg="1">
           <b-icon icon="x-lg" @click="resetSearch"></b-icon>
-        </div>
-      </div>
+
+        </b-col>
+        <b-col class="text-end">
+          <b-button variant="outline-secondary" class="m-1" size="sm"> Importar
+            <b-icon icon="file-excel" variant="success"></b-icon>
+          </b-button>
+
+          <b-button variant="outline-secondary" size="sm"> Exportar
+            <b-icon icon="file-excel" variant="success"></b-icon>
+          </b-button>
+        </b-col>
+
+
+      </b-row>
     </div>
 
     <b-table class="mt-3" :items="paginatedClients" :fields="fields" responsive="sm" striped hover>
+
+      <template #cell(index)="data">
+        {{ (currentPage - 1) * perPage + data.index + 1 }}
+      </template>
+      <template #cell(obligadoContabilidad)="data">
+        <div class="d-flex justify-content-center align-items-center">
+
+          <b-icon icon="circle-fill" variant="success" v-if="data.item.obligadoContabilidad === 'SI'"></b-icon>
+          <b-icon v-else icon="circle-fill" variant="danger"></b-icon>
+        </div>
+      </template>
+
       <template #cell(actions)="data">
-        <b-button variant="primary" size="sm" @click="openModal('editar', data.item)">Editar</b-button>
+        <b-button variant="primary" size="sm" @click="showModalEdit(data.item)">Editar</b-button>
         <b-button variant="danger" size="sm" @click="deleteClient(data.item._id)">Eliminar</b-button>
       </template>
     </b-table>
 
     <b-pagination
         v-model="currentPage"
-        :total-rows="clients.length"
+        :total-rows="filteredClients.length"
         :per-page="perPage"
         align="center"
         class="mt-3"
     ></b-pagination>
 
-    <b-modal id="modal-nuevo-cliente" title="Nuevo Cliente" centered size="xl">
-      <b-form @submit.stop.prevent="saveNewClient">
+    <b-modal ref="my-modal" :title="editMode?'Edita cliente':'Nuevo cliente' " size="xl" centered hide-header-close>
+
+
+      <b-form @submit.stop.prevent="saveClient">
         <div class="row mb-3">
           <b-form-group label="Razón Social" label-for="razonSocial" class="col-12">
             <b-form-input v-model="clienteActual.razonSocial" id="razonSocial" required></b-form-input>
@@ -227,12 +319,15 @@ export default {
           </b-form-group>
         </div>
       </b-form>
-
       <template #modal-footer>
-        <b-button variant="secondary" @click="$bvModal.hide('modal-nuevo-cliente')">Cancelar</b-button>
-        <b-button variant="primary" @click="saveClient">Guardar</b-button>
+
+        <b-button class="mt-2" variant="outline-secondary" block @click="hideModal">Cancelar</b-button>
+        <b-button class="mt-2" v-if="!editMode" variant="outline-success" block @click="saveClient">Guardar cliente
+        </b-button>
+        <b-button class="mt-2" v-if="editMode" variant="outline-success" block @click="saveClient">Actualizar cliente
+        </b-button>
+
       </template>
     </b-modal>
   </b-container>
 </template>
-
