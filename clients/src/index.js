0d81efafc59d3dc096fbf54d659f9290585727c3
cxzadm <113950353+cxzadm@users.noreply.github.com>
2024-10-09 18:54:55 -0500

    add clients

--- clients/src/index.js
+++ clients/src/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors'); 
 const clientRoutes = require('./routes/clientRoutes');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3005;
+
+
+app.use(cors({
+    origin: 'http://localhost:8080', // Cambia este valor si es necesario, o usa '*' para permitir cualquier origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+  }));
 
 app.use(express.json());
 app.use('/api/clients', clientRoutes);
