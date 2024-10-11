cdde386777217b4bb530d2c773969d5e19721daf
cxzadm <113950353+cxzadm@users.noreply.github.com>
2024-10-10 19:57:33 -0500

    add Front suppliers

--- suppliers/src/app.js
+++ suppliers/src/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const supplierRoutes = require('./routes/supplierRoutes');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3003;
+
+app.use(cors({
+    origin: 'http://localhost:8080', // Cambia este valor si es necesario, o usa '*' para permitir cualquier origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+}));
 
 app.use(express.json());
 app.use('/api/suppliers', supplierRoutes);
